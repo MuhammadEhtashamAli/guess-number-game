638c654c208fab536fa457e6192bd5515f231efa
MuhammadEhtashamAli <www.alirasheeed102030@gmail.com>
2024-03-30 01:02:26 +0500

    This is use to Guess game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer" 
+
+const randomNumber =Math.floor(Math.random()*6 + 1);
+
+const answer = await inquirer.prompt([{
+
+name:"userGuessedNumber",
+type :"number",
+message:"Choose a random Number",
+
+},
+]);
+if(answer.userGuessedNumber === randomNumber ){
+    console.log("Congralation !! You guess a Right Number!!!");
+}
+else{
+    console.log("sorry you guess wrong number");
+}
